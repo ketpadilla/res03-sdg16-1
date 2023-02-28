9334d6f397cadac6167c7dbf06fb7ee21c40d46a
Kristian Emmanuel Padilla <ketpadilla05@gmail.com>
2023-02-28 17:36:37 +0800

    day2

---
+++ day2/es6/es6.js
@@ -0,0 +1,80 @@
+// Statements, semicolons, comments
+
+/*
+ Multiline
+ comments
+*/
+
+// console.log('Hello')
+// console.log('world')
+
+// Variables
+// var a = 1
+// var b = 2
+// var c = 3
+// let mes = 'Welcome to the Matrix'
+// console.log(mes + ' ' + (a+b+c))
+// console.log(mes,a+b+c)
+
+// Data Types
+// console.log("a + 7 =",a + 7)
+// console.log("b + 7 =",b + 7)
+// console.log(Number.MAX_VALUE)
+
+// String
+// let stn = "Hello, world!"
+// console.log(stn)
+
+// Boolean
+// let isGreater = 4 > 1
+// console.log(isGreater)
+
+// Object
+// let Object = [0, 1, 2, 3]
+// console.log(Object[0])
+// console.log(Object[1])
+// console.log(Object)
+// console.log(typeof Object)
+// console.log(Object.length)
+
+// Operations
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// const car = {
+//     make: 'Honda',
+//     model: 'Accord',
+//     year: 1998}
+// console.log('make' in car)
+// console.log('color' in car)
+// console.log(10 == '10')
+// console.log(10 === '10')
+// console.log(true && true)
+// console.log(false && true)
+// console.log(false || true)
+
+// Conditional
+// if (a == 1) {
+//     console.log('a is equal to 1')
+// } else {
+//     console.log('a is not equal to 1') }
+// for (var i = 0; i <= b; i++) {
+//     console.log(i)
+// }
+// const Object = [1,5,9,100]
+// for (i in Object) {
+//     console.log(Object[i])} 
+// for (i of Object) {
+//     console.log(i)}
+// Object.forEach(function(value) {
+//     console.log(value)})
+
+// Function
+function demo () {
+    console.log('Hello, world!')
+}
+
+demo()
